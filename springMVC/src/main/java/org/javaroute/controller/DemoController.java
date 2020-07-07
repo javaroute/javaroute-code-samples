f515dec7419b26f7d52181e415a0768d23cde708
wangfa <wangfuan_email@163.com>
2020-07-07 09:28:26 +0800

    请求方法限定

--- springMVC/src/main/java/org/javaroute/controller/DemoController.java
+++ springMVC/src/main/java/org/javaroute/controller/DemoController.java
@@ -2,12 +2,18 @@ package org.javaroute.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/demo")
 @Controller
+@RequestMapping("/demo")
 public class DemoController {
     @RequestMapping("/v1")
     public String demo1(){
       return "hello";
+    }
+
+    @RequestMapping(value="/v2",method = RequestMethod.POST)
+    public String demo2(){
+        return "hello";
     }
 }
